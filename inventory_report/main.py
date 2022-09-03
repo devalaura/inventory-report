10d6e3ceb110cc68cdecf156cf272e4f6c201833
anaalvsr <analauraalves25@gmail.com>
2022-09-03 16:15:50 -0300

    feat: cria método main para controlar aplicação

--- inventory_report/main.py
+++ inventory_report/main.py
@@ -1,2 +1,30 @@
+import sys
+import pathlib
+
+from inventory_report.inventory.inventory_refactor import InventoryRefactor
+from inventory_report.importer.csv_importer import CsvImporter
+from inventory_report.importer.json_importer import JsonImporter
+from inventory_report.importer.xml_importer import XmlImporter
+
+
 def main():
-    pass
+    if len(sys.argv) != 3:
+        return print("Verifique os argumentos", file=sys.stderr)
+    else:
+        file_name = sys.argv[1]
+        report_type = sys.argv[2]
+        file_ext = pathlib.Path(file_name).suffix
+        if file_ext == '.csv':
+            inventory = InventoryRefactor(CsvImporter)
+            report_data = inventory.import_data(file_name, report_type)
+            return sys.stdout.write(report_data)
+        elif file_ext == '.json':
+            inventory = InventoryRefactor(JsonImporter)
+            report_data = inventory.import_data(file_name, report_type)
+            return sys.stdout.write(report_data)
+        elif file_ext == '.xml':
+            inventory = InventoryRefactor(XmlImporter)
+            report_data = inventory.import_data(file_name, report_type)
+            return sys.stdout.write(report_data)
+        else:
+            raise ValueError("Arquivo não suportado.")
